158e8e994adb0a0fb425b95b6f25aafda52319c2
= <7rubengarcia7@gmail.com>
2021-05-28 15:11:04 -0700

    feat(atoms): add 'aria-*' attributes and role of 'switch' to '<SwitchButton />'

--- src/components/atoms/SwitchButton/index.tsx
+++ src/components/atoms/SwitchButton/index.tsx
@@ -3,17 +3,22 @@ import React, { FC } from 'react';
 import './styles.scss';
 
 interface SwitchButtonProps {
+  'aria-describedby'?: string;
+  'aria-label'?: string;
   isActive: boolean;
   onChange: () => void;
 }
 
 const SwitchButton: FC<SwitchButtonProps> = props => {
-  const { isActive = false, onChange } = props;
+  const { isActive = false, onChange, ...rest } = props;
 
   return (
     <button
+      {...rest}
+      aria-checked={isActive}
       className="switch-button"
       onClick={onChange}
+      role="switch"
       style={{
         backgroundColor: isActive
           ? 'var(--color-brand-blue100)'
