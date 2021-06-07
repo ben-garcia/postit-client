b0b41a37cdebb2f0bb082318d8e960221080ef36
= <7rubengarcia7@gmail.com>
2021-06-07 12:09:03 -0700

    feat(pages): add 'login'

---
+++ src/pages/account/login.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import { Login as LoginPage } from '../../components/pages';
+
+interface LoginProps {}
+
+const Login: FC<LoginProps> = () => <LoginPage />;
+
+export default Login;
