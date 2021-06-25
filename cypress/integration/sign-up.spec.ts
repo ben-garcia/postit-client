7c0a33ae8dbce7e2e4b1455e4c43d703ff7d58d3
= <7rubengarcia7@gmail.com>
2021-06-25 12:17:29 -0700

    test(cypress): add 'signUp' tests

--- cypress/integration/sign-up.spec.ts
+++ cypress/integration/sign-up.spec.ts
@@ -1,47 +1,37 @@
 context('Sign Up', () => {
-	it('should successfully register a user', () => {
-		const email = 'bestemail@email.com';
-		const username = 'benben';
-		const password = 'password';
+  it('should successfully register a user', () => {
+    const email = 'bestemail@email.com';
+    const username = 'benben';
+    const password = 'password';
 
-		cy.clearDB()
-      .visit('/')
-      .contains('Sign Up')
-      .click();
+    cy.clearDB().visit('/').contains('Sign Up').click();
 
+    cy.get('input[name="email"]').type(email).should('have.value', email);
 
-		cy.get('input[name="email"]')
-			.type(email)
-			.should('have.value', email);
+    cy.get('input[name="username"]')
+      .type(username, { force: true })
+      .should('have.value', username);
 
-		cy.get('input[name="username"]')
-			.type(username, { force: true })
-			.should('have.value', username);
+    cy.get('input[name="password"]')
+      .type(password)
+      .should('have.value', password);
 
-		cy.get('input[name="password"]')
-			.type(password)
-			.should('have.value', password);
+    cy.get('button[type="submit"]').click();
+  });
 
-		cy.get('button[type="submit"]')
-			.click();
-	});
+  it('should show error message when username is already taken', () => {
+    const user = {
+      password: 'benbenben',
+      username: 'benben',
+    };
 
-	it('should show error message when username is already taken', () => {
-		const user = {
-			password: 'benbenben',
-			username: 'benben',
-		};
+    cy.clearDB().signUpUser(user).visit('/').contains('Sign Up').click();
 
-		cy.clearDB()
-			.signUpUser(user)
-      .visit('/')
-      .contains('Sign Up')
-      .click();
+    cy.get('input[name="username"]').type(user.username, { force: true });
 
-		cy.get('input[name="username"]')
-			.type(user.username, { force: true })
-
-		cy.get('div[aria-live="polite"]')
-			.should('have.text', 'This username is already taken.');
-	});
+    cy.get('div[aria-live="polite"]').should(
+      'have.text',
+      'This username is already taken.'
+    );
+  });
 });
