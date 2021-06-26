f5c33608523a98b66c495eee72737a23ad6a64d8
= <7rubengarcia7@gmail.com>
2021-06-26 13:23:20 -0700

    chore(usedebounce): remove 'fn' param

--- src/hooks/use-debounce.ts
+++ src/hooks/use-debounce.ts
@@ -4,18 +4,16 @@ import { useEffect, useState } from 'react';
  * Hook that delays the execution of a function.
  *
  * @param val the user input value
- * @param fn the function to be executed
  * @param timeout the amount of time to delay
  *
  * @return val
  */
-const useDebounce = <T>(val: T, fn: () => void, timeout?: number) => {
+const useDebounce = <T>(val: T, timeout?: number) => {
   const [value, setValue] = useState<T>(val);
 
   useEffect(() => {
     const timerId = setTimeout(() => {
       setValue(val);
-      fn();
     }, timeout ?? 500);
 
     return () => {
