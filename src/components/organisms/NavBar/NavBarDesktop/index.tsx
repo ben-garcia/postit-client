247de6dcc7d489a3a509b8e5b3e17dad87277341
= <7rubengarcia7@gmail.com>
2021-07-02 12:11:20 -0700

    feat(navbardesktop): add 'user.isLoggedIn' support

--- src/components/organisms/NavBar/NavBarDesktop/index.tsx
+++ src/components/organisms/NavBar/NavBarDesktop/index.tsx
@@ -1,8 +1,20 @@
 import React, { FC, useState } from 'react';
+import { AddIcon, EnvelopIcon } from 'supernova-ui';
 
-import { Button, LogoIcon, LogoTextIcon, SearchBar } from '../../../atoms';
+import {
+  Button,
+  CameraIcon,
+  ChatIcon,
+  CoinIcon,
+  LogoIcon,
+  LogoTextIcon,
+  RisingIcon,
+  TopIcon,
+  SearchBar,
+} from '../../../atoms';
 import { UserMenu } from '../../../molecules';
 import { LoginModal, SignUpModal } from '../..';
+import { useUser } from '../../../../hooks';
 
 /**
  * comment out for Next
@@ -16,6 +28,9 @@ interface NavBarDesktopProps {}
 const NavBarDesktop: FC<NavBarDesktopProps> = () => {
   const [loginModalIsOpen, setLoginModalIsOpen] = useState(false);
   const [signUpModalIsOpen, setSignUpModalIsOpen] = useState(false);
+  const {
+    state: { user },
+  } = useUser();
 
   return (
     <header className="navbar-desktop">
@@ -25,20 +40,78 @@ const NavBarDesktop: FC<NavBarDesktopProps> = () => {
       </div>
       <SearchBar />
       <div className="navbar-desktop__wrapper">
-        <Button
-          margin="0 xs"
-          onClick={() => setLoginModalIsOpen(true)}
-          secondary
-        >
-          Log In
-        </Button>
-        <Button
-          margin="0 sm 0 xs"
-          onClick={() => setSignUpModalIsOpen(true)}
-          primary
-        >
-          Sign Up
-        </Button>
+        {!user.isLoggedIn && (
+          <>
+            <Button
+              margin="0 xs"
+              onClick={() => setLoginModalIsOpen(true)}
+              secondary
+            >
+              Log In
+            </Button>
+            <Button
+              margin="0 sm 0 xs"
+              onClick={() => setSignUpModalIsOpen(true)}
+              primary
+            >
+              Sign Up
+            </Button>
+          </>
+        )}
+        {user.isLoggedIn && (
+          <>
+            <Button
+              className="navbar-desktop__button"
+              hoverColor="var(--color-brand-grey100)"
+              secondary
+            >
+              <RisingIcon size="1.2rem" />
+            </Button>
+            <Button
+              className="navbar-desktop__button"
+              hoverColor="var(--color-brand-grey100)"
+              secondary
+            >
+              <TopIcon size="1.2rem" />
+            </Button>
+            <Button
+              className="navbar-desktop__button"
+              hoverColor="var(--color-brand-grey100)"
+              secondary
+            >
+              <CameraIcon size="1.2rem" />
+            </Button>
+            <Button
+              className="navbar-desktop__button"
+              hoverColor="var(--color-brand-grey100)"
+              secondary
+            >
+              <ChatIcon size="1.2rem" />
+            </Button>
+            <Button
+              className="navbar-desktop__button"
+              hoverColor="var(--color-brand-grey100)"
+              secondary
+            >
+              <EnvelopIcon size="1.2rem" />
+            </Button>
+            <Button
+              className="navbar-desktop__button"
+              hoverColor="var(--color-brand-grey100)"
+              secondary
+            >
+              <AddIcon size="1.2rem" />
+            </Button>
+            <Button
+              className="navbar-desktop__coins-button"
+              hoverColor="var(--color-brand-grey100)"
+              secondary
+            >
+              <CoinIcon margin="0 0.2rem 0 0" size="1.3rem" />
+              Get Coins
+            </Button>
+          </>
+        )}
         <UserMenu />
       </div>
       {loginModalIsOpen && !signUpModalIsOpen && (
--- src/components/organisms/NavBar/stories.tsx
+++ src/components/organisms/NavBar/stories.tsx
@@ -2,10 +2,30 @@ import { Meta } from '@storybook/react';
 import React from 'react';
 
 import NavBar from '.';
+import { UserContext } from '../../../contexts';
 
 export default {
   component: NavBar,
   title: 'Postit/Organism/NavBar',
 } as Meta;
 
-export const Default = () => <NavBar />;
+export const LoggedOut = () => (
+  <UserContext.Provider
+    value={{
+      dispatch: () => {},
+      state: { user: { isLoggedIn: false } },
+    }}
+  >
+    <NavBar />
+  </UserContext.Provider>
+);
+export const LoggedIn = () => (
+  <UserContext.Provider
+    value={{
+      dispatch: () => {},
+      state: { user: { isLoggedIn: true, username: 'testuser' } },
+    }}
+  >
+    <NavBar />
+  </UserContext.Provider>
+);
