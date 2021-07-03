5610f33d4b2d9add978d1ce7224dc8b71fc4140a
= <7rubengarcia7@gmail.com>
2021-07-03 08:48:35 -0700

    chore(index): check local storage for user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
+
 import { NavBar } from '../components/organisms';
+import { useUser } from '../hooks';
 
 export default function Home() {
+  const { dispatch } = useUser();
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+
+    if (user) {
+      const parsedUser = JSON.parse(user);
+
+      dispatch({
+        type: 'USER_LOGGED_IN',
+        payload: parsedUser.username,
+      });
+    }
+  }, []);
   return (
     <div className="home-page">
       <NavBar />
