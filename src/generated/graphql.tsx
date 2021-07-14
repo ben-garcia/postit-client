ba9b8e949dcae6587e061e942426573b70f697d3
= <7rubengarcia7@gmail.com>
2021-07-14 14:54:35 -0700

    feat(graphql): add 'createCommunity' mutation and 'isCommunityNameUnique' query

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -18,11 +18,42 @@ export type Scalars = {
   Float: number;
 };
 
+export type AuthErrorConstraints = {
+  __typename?: 'AuthErrorConstraints';
+  isEmail?: Maybe<Scalars['String']>;
+  maxLength?: Maybe<Scalars['String']>;
+  minLength?: Maybe<Scalars['String']>;
+};
+
+export type AuthErrors = {
+  __typename?: 'AuthErrors';
+  constraints: AuthErrorConstraints;
+  field: Scalars['String'];
+};
+
 export enum ChatRequestsFrom {
   Everyone = 'EVERYONE',
   Days = 'DAYS',
   Nobody = 'NOBODY',
 }
+
+export type Community = {
+  __typename?: 'Community';
+  id: Scalars['ID'];
+  bannerUrl?: Maybe<Scalars['String']>;
+  coinCount: Scalars['Int'];
+  creator: User;
+  description?: Maybe<Scalars['String']>;
+  iconUrl?: Maybe<Scalars['String']>;
+  isNsfw: Scalars['String'];
+  location?: Maybe<Scalars['String']>;
+  name: Scalars['String'];
+  themeColor?: Maybe<Scalars['String']>;
+  topics?: Maybe<Scalars['String']>;
+  type: Scalars['String'];
+  createdAt: Scalars['String'];
+  updatedAt: Scalars['String'];
+};
 
 export enum CommunityContentSort {
   Hot = 'HOT',
@@ -30,6 +61,31 @@ export enum CommunityContentSort {
   New = 'NEW',
   Top = 'TOP',
 }
+
+export type CreateCommunityError = {
+  __typename?: 'CreateCommunityError';
+  constraints: CreateCommunityErrorConstraints;
+  field: Scalars['String'];
+};
+
+export type CreateCommunityErrorConstraints = {
+  __typename?: 'CreateCommunityErrorConstraints';
+  matches?: Maybe<Scalars['String']>;
+  maxLength?: Maybe<Scalars['String']>;
+  minLength?: Maybe<Scalars['String']>;
+};
+
+export type CreateCommunityInput = {
+  name: Scalars['String'];
+  isNsfw: Scalars['Boolean'];
+  type: Scalars['String'];
+};
+
+export type CreateCommunityResponse = {
+  __typename?: 'CreateCommunityResponse';
+  errors?: Maybe<Array<CreateCommunityError>>;
+  created?: Maybe<Scalars['Boolean']>;
+};
 
 export type EmailNotificationPreferences = {
   __typename?: 'EmailNotificationPreferences';
@@ -67,6 +123,7 @@ export type LogInInput = {
 
 export type LogInResponse = {
   __typename?: 'LogInResponse';
+  errors?: Maybe<Array<AuthErrors>>;
   success?: Maybe<Scalars['Boolean']>;
 };
 
@@ -74,6 +131,7 @@ export type Mutation = {
   __typename?: 'Mutation';
   logIn: LogInResponse;
   signUp: SignUpResponse;
+  createCommunity: CreateCommunityResponse;
 };
 
 export type MutationLogInArgs = {
@@ -82,6 +140,10 @@ export type MutationLogInArgs = {
 
 export type MutationSignUpArgs = {
   createUserData: SignUpInput;
+};
+
+export type MutationCreateCommunityArgs = {
+  createCommunityData: CreateCommunityInput;
 };
 
 export type NotificationPreferences = {
@@ -142,25 +204,17 @@ export type Profile = {
 
 export type Query = {
   __typename?: 'Query';
+  isCommunityNameUnique: Scalars['Boolean'];
   getAllUsers: Array<User>;
   isUsernameUnique: Scalars['Boolean'];
 };
 
+export type QueryIsCommunityNameUniqueArgs = {
+  name: Scalars['String'];
+};
+
 export type QueryIsUsernameUniqueArgs = {
   username: Scalars['String'];
-};
-
-export type SignUpError = {
-  __typename?: 'SignUpError';
-  constraints: SignUpErrorConstraints;
-  field: Scalars['String'];
-};
-
-export type SignUpErrorConstraints = {
-  __typename?: 'SignUpErrorConstraints';
-  isEmail?: Maybe<Scalars['String']>;
-  maxLength?: Maybe<Scalars['String']>;
-  minLength?: Maybe<Scalars['String']>;
 };
 
 export type SignUpInput = {
@@ -171,13 +225,14 @@ export type SignUpInput = {
 
 export type SignUpResponse = {
   __typename?: 'SignUpResponse';
-  errors?: Maybe<Array<SignUpError>>;
+  errors?: Maybe<Array<AuthErrors>>;
   created?: Maybe<Scalars['Boolean']>;
 };
 
 export type User = {
   __typename?: 'User';
   id: Scalars['ID'];
+  communities: Array<Community>;
   email?: Maybe<Scalars['String']>;
   emailNotificationPreferences?: Maybe<EmailNotificationPreferences>;
   generalPreferences?: Maybe<GeneralPreferences>;
@@ -186,6 +241,35 @@ export type User = {
   profile?: Maybe<Profile>;
   createdAt: Scalars['String'];
   updatedAt: Scalars['String'];
+};
+
+export type CreateCommunityMutationVariables = Exact<{
+  name: Scalars['String'];
+  isNsfw: Scalars['Boolean'];
+  type: Scalars['String'];
+}>;
+
+export type CreateCommunityMutation = { __typename?: 'Mutation' } & {
+  createCommunity: { __typename?: 'CreateCommunityResponse' } & Pick<
+    CreateCommunityResponse,
+    'created'
+  > & {
+      errors?: Maybe<
+        Array<
+          { __typename?: 'CreateCommunityError' } & Pick<
+            CreateCommunityError,
+            'field'
+          > & {
+              constraints: {
+                __typename?: 'CreateCommunityErrorConstraints';
+              } & Pick<
+                CreateCommunityErrorConstraints,
+                'minLength' | 'matches' | 'maxLength'
+              >;
+            }
+        >
+      >;
+    };
 };
 
 export type LogInMutationVariables = Exact<{
@@ -210,9 +294,9 @@ export type SignUpMutation = { __typename?: 'Mutation' } & {
   > & {
       errors?: Maybe<
         Array<
-          { __typename?: 'SignUpError' } & Pick<SignUpError, 'field'> & {
-              constraints: { __typename?: 'SignUpErrorConstraints' } & Pick<
-                SignUpErrorConstraints,
+          { __typename?: 'AuthErrors' } & Pick<AuthErrors, 'field'> & {
+              constraints: { __typename?: 'AuthErrorConstraints' } & Pick<
+                AuthErrorConstraints,
                 'isEmail' | 'maxLength' | 'minLength'
               >;
             }
@@ -220,6 +304,15 @@ export type SignUpMutation = { __typename?: 'Mutation' } & {
       >;
     };
 };
+
+export type IsCommunityNameUniqueQueryVariables = Exact<{
+  name: Scalars['String'];
+}>;
+
+export type IsCommunityNameUniqueQuery = { __typename?: 'Query' } & Pick<
+  Query,
+  'isCommunityNameUnique'
+>;
 
 export type IsUsernameUniqueQueryVariables = Exact<{
   username: Scalars['String'];
@@ -230,6 +323,67 @@ export type IsUsernameUniqueQuery = { __typename?: 'Query' } & Pick<
   'isUsernameUnique'
 >;
 
+export const CreateCommunityDocument = gql`
+  mutation CreateCommunity($name: String!, $isNsfw: Boolean!, $type: String!) {
+    createCommunity(
+      createCommunityData: { name: $name, isNsfw: $isNsfw, type: $type }
+    ) {
+      errors {
+        field
+        constraints {
+          minLength
+          matches
+          maxLength
+        }
+      }
+      created
+    }
+  }
+`;
+export type CreateCommunityMutationFn = Apollo.MutationFunction<
+  CreateCommunityMutation,
+  CreateCommunityMutationVariables
+>;
+
+/**
+ * __useCreateCommunityMutation__
+ *
+ * To run a mutation, you first call `useCreateCommunityMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateCommunityMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createCommunityMutation, { data, loading, error }] = useCreateCommunityMutation({
+ *   variables: {
+ *      name: // value for 'name'
+ *      isNsfw: // value for 'isNsfw'
+ *      type: // value for 'type'
+ *   },
+ * });
+ */
+export function useCreateCommunityMutation(
+  baseOptions?: Apollo.MutationHookOptions<
+    CreateCommunityMutation,
+    CreateCommunityMutationVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useMutation<
+    CreateCommunityMutation,
+    CreateCommunityMutationVariables
+  >(CreateCommunityDocument, options);
+}
+export type CreateCommunityMutationHookResult = ReturnType<
+  typeof useCreateCommunityMutation
+>;
+export type CreateCommunityMutationResult = Apollo.MutationResult<CreateCommunityMutation>;
+export type CreateCommunityMutationOptions = Apollo.BaseMutationOptions<
+  CreateCommunityMutation,
+  CreateCommunityMutationVariables
+>;
 export const LogInDocument = gql`
   mutation LogIn($username: String!, $password: String!) {
     logIn(logInData: { username: $username, password: $password }) {
@@ -340,6 +494,62 @@ export type SignUpMutationResult = Apollo.MutationResult<SignUpMutation>;
 export type SignUpMutationOptions = Apollo.BaseMutationOptions<
   SignUpMutation,
   SignUpMutationVariables
+>;
+export const IsCommunityNameUniqueDocument = gql`
+  query IsCommunityNameUnique($name: String!) {
+    isCommunityNameUnique(name: $name)
+  }
+`;
+
+/**
+ * __useIsCommunityNameUniqueQuery__
+ *
+ * To run a query within a React component, call `useIsCommunityNameUniqueQuery` and pass it any options that fit your needs.
+ * When your component renders, `useIsCommunityNameUniqueQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useIsCommunityNameUniqueQuery({
+ *   variables: {
+ *      name: // value for 'name'
+ *   },
+ * });
+ */
+export function useIsCommunityNameUniqueQuery(
+  baseOptions: Apollo.QueryHookOptions<
+    IsCommunityNameUniqueQuery,
+    IsCommunityNameUniqueQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<
+    IsCommunityNameUniqueQuery,
+    IsCommunityNameUniqueQueryVariables
+  >(IsCommunityNameUniqueDocument, options);
+}
+export function useIsCommunityNameUniqueLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    IsCommunityNameUniqueQuery,
+    IsCommunityNameUniqueQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<
+    IsCommunityNameUniqueQuery,
+    IsCommunityNameUniqueQueryVariables
+  >(IsCommunityNameUniqueDocument, options);
+}
+export type IsCommunityNameUniqueQueryHookResult = ReturnType<
+  typeof useIsCommunityNameUniqueQuery
+>;
+export type IsCommunityNameUniqueLazyQueryHookResult = ReturnType<
+  typeof useIsCommunityNameUniqueLazyQuery
+>;
+export type IsCommunityNameUniqueQueryResult = Apollo.QueryResult<
+  IsCommunityNameUniqueQuery,
+  IsCommunityNameUniqueQueryVariables
 >;
 export const IsUsernameUniqueDocument = gql`
   query IsUsernameUnique($username: String!) {
