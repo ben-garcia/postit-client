b51688c49d0c5a9a5ad81b5720c47af13c34a2ff
= <7rubengarcia7@gmail.com>
2021-07-15 13:58:47 -0700

    chore(navbar): add effect to check local storage for 'user'

--- src/components/organisms/NavBar/index.tsx
+++ src/components/organisms/NavBar/index.tsx
@@ -1,14 +1,29 @@
-import React, { FC } from 'react';
+import React, { useEffect, FC } from 'react';
 
 import { useBreakpoint } from 'supernova-ui';
 
 import NavBarDesktop from './NavBarDesktop';
 import NavBarMobile from './NavBarMobile';
+import { useUser } from '../../../hooks';
 
 interface NavBarProps {}
 
 const NavBar: FC<NavBarProps> = () => {
   const breakpoint = useBreakpoint();
+  const { dispatch } = useUser();
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+
+    if (user) {
+      const parsedUser = JSON.parse(user);
+
+      dispatch({
+        type: 'USER_LOGGED_IN',
+        payload: parsedUser.username,
+      });
+    }
+  }, []);
 
   return (
     <>
