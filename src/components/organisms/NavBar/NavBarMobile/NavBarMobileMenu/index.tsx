cfb312e47eb7c98180f3397c3097c643e760dbb8
= <7rubengarcia7@gmail.com>
2021-07-16 11:14:39 -0700

    chore(navbarmobilemenu): render correctly when 'user.isLoggedIn' is true

--- src/components/organisms/NavBar/NavBarMobile/NavBarMobileMenu/index.tsx
+++ src/components/organisms/NavBar/NavBarMobile/NavBarMobileMenu/index.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import Link from 'next/link';
 
 import { ChevronDownIcon, InfoIcon, SettingsIcon, Text } from 'supernova-ui';
-import { Button, RisingIcon, SearchBar } from '../../../../atoms';
+import { Button, ChatIcon, RisingIcon, SearchBar } from '../../../../atoms';
 import { useUser } from '../../../../../hooks';
 
 /**
@@ -24,7 +24,16 @@ const NavBarMobileMenu: FC<NavBarMobileMenuProps> = () => {
       <SearchBar className="mobile-menu__searchbar" fill="white" />
       <div className="menu-list">
         {user.isLoggedIn && (
-          <Text className="mobile-menu__username">{user.username}</Text>
+          <>
+            <Text className="mobile-menu__username">{user.username}</Text>
+            <div className="menu-list__item">
+              <div className="menu-list__wrapper">
+                <InfoIcon fill="white" margin="0 0.5rem 0 0" size="1rem" />
+                <Text>Inbox</Text>
+              </div>
+              <ChevronDownIcon fill="white" size="0.7rem" />
+            </div>
+          </>
         )}
         <div className="menu-list__item">
           <div className="menu-list__wrapper">
@@ -33,26 +42,43 @@ const NavBarMobileMenu: FC<NavBarMobileMenuProps> = () => {
           </div>
           <ChevronDownIcon fill="white" size="0.7rem" />
         </div>
+        {!user.isLoggedIn && (
+          <>
+            <div className="menu-list__item">
+              <div className="menu-list__wrapper">
+                <InfoIcon fill="white" margin="0 0.5rem 0 0" size="1rem" />
+                <Text>Explore</Text>
+              </div>
+              <ChevronDownIcon fill="white" size="0.7rem" />
+            </div>
+            <div className="menu-list__item">
+              <div className="menu-list__wrapper">
+                <RisingIcon fill="white" margin="0 0.5rem 0 0" size="1rem" />
+                <Text>Popular Posts</Text>
+              </div>
+              <ChevronDownIcon fill="white" size="0.7rem" />
+            </div>
+          </>
+        )}
+        {user.isLoggedIn && (
+          <div className="menu-list__item">
+            <div className="menu-list__wrapper">
+              <SettingsIcon fill="white" margin="0 0.5rem 0 0" size="1rem" />
+              <Text>My Communities</Text>
+            </div>
+          </div>
+        )}
         <div className="menu-list__item">
           <div className="menu-list__wrapper">
-            <InfoIcon fill="white" margin="0 0.5rem 0 0" size="1rem" />
-            <Text>Explore</Text>
+            <ChatIcon fill="white" margin="0 0.5rem 0 0" size="1rem" />
+            <Text>Chat</Text>
           </div>
-          <ChevronDownIcon fill="white" size="0.7rem" />
         </div>
         <div className="menu-list__item">
           <div className="menu-list__wrapper">
-            <RisingIcon fill="white" margin="0 0.5rem 0 0" size="1rem" />
-            <Text>Popular Posts</Text>
+            <SettingsIcon fill="white" margin="0 0.5rem 0 0" size="1rem" />
+            <Text>Saved</Text>
           </div>
-          <ChevronDownIcon fill="white" size="0.7rem" />
-        </div>
-        <div className="menu-list__item">
-          <div className="menu-list__wrapper">
-            <InfoIcon fill="white" margin="0 0.5rem 0 0" size="1rem" />
-            <Text>About Postit</Text>
-          </div>
-          <ChevronDownIcon fill="white" size="0.7rem" />
         </div>
         <div className="menu-list__item">
           <div className="menu-list__wrapper">
@@ -61,12 +87,40 @@ const NavBarMobileMenu: FC<NavBarMobileMenuProps> = () => {
           </div>
           <ChevronDownIcon fill="white" size="0.7rem" />
         </div>
+        <div className="menu-list__item">
+          <div className="menu-list__wrapper">
+            <RisingIcon fill="white" margin="0 0.5rem 0 0" size="1rem" />
+            <Text>r/popular</Text>
+          </div>
+        </div>
+        <div className="menu-list__item">
+          <div className="menu-list__wrapper">
+            <SettingsIcon fill="white" margin="0 0.5rem 0 0" size="1rem" />
+            <Text>r/all</Text>
+          </div>
+        </div>
       </div>
-      <Link href="/register">
-        <Button className="mobile-menu__cta" primary>
-          Log In / Sign Up
-        </Button>
-      </Link>
+      <div className="menu-list__item">
+        <div className="menu-list__wrapper">
+          <InfoIcon fill="white" margin="0 0.5rem 0 0" size="1rem" />
+          <Text>About Postit</Text>
+        </div>
+        <ChevronDownIcon fill="white" size="0.7rem" />
+      </div>
+      {!user.isLoggedIn && (
+        <Link href="/register">
+          <Button className="mobile-menu__cta" primary>
+            Log In / Sign Up
+          </Button>
+        </Link>
+      )}
+      {user.isLoggedIn && (
+        <Link href="/communities/create">
+          <Button className="mobile-menu__cta" primary>
+            Create a Community
+          </Button>
+        </Link>
+      )}
     </div>
   );
 };
